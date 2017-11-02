fa92149f97e58b478659ddefa0c47c7d99447f3a
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:22:46 -0400

    First commit

---
+++ CS 10/CS 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> customers = new List<string>();
+            customers.Add("Bob"); //0
+            customers.Add("Mary"); //1
+            //Here
+            customers.Add("Chad"); //2
+
+            customers.Insert(2, "Heather");
+
+            foreach (string customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+
+            List<int> ages = new List<int>();
+            ages.Add(10);
+            ages.Add(10);
+            ages.Add(10);
+            ages.Add(10);
+            ages.Add(10);
+            ages.Add(10);
+            ages.Add(10);
+
+            Console.Read();
+        }
+    }
+}
